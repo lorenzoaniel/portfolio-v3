49f89dca8def07ef6e0fba87bfe05714b3c66213
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-08 13:06:27 -0600

    Routes and Navbar structure
    to make it more modular I am trying to place components in APP, I will try and use context to simulate a 'store' where states can be global

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link } from "react-router-dom";
+import { Routes, Route } from "react-router";
+import { useState } from "react";
+import { routesPageData } from "./Api/RoutesPageData/routesPageData";
+import { GlobalStyleApp } from "./Styles/GlobalStyleApp/GlobalStyleApp";
+
+import About from "./Routes/About/About";
+import Contact from "./Routes/Contact/Contact";
+import Projects from "./Routes/Projects/Projects";
+import NoMatch from "./Routes/NoMatch/NoMatch";
+import Layout from "./Routes/Layout/Layout";
+import NavBar from "./Components/Navbar/NavBar";
+import OutletContainer from "./Components/Outlet/OutletContainer";
+import NavbarMenutItem from "./Components/Navbar/NavbarMenuItem/NavbarMenutItem";
+
+// const routesPageDataContext = createContext(routesPageData);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [routePageData] = useState(routesPageData);
+
+	return (
+		<BrowserRouter>
+			<GlobalStyleApp />
+			<Routes>
+				<Route
+					path={routePageData.layout.pagePath}
+					element={
+						<Layout classNameProp={routePageData.layout.classNameMenuItem}>
+							<NavBar>
+								<NavbarMenutItem classNameProp={routePageData.about.classNameMenuItem}>
+									<Link to={routePageData.about.pagePath}>{routePageData.about.pageTitle}</Link>
+								</NavbarMenutItem>
+								<NavbarMenutItem classNameProp={routePageData.projects.classNameMenuItem}>
+									<Link to={routePageData.contact.pagePath}>{routePageData.contact.pageTitle}</Link>
+								</NavbarMenutItem>
+								<NavbarMenutItem classNameProp={routePageData.contact.classNameMenuItem}>
+									<Link to={routePageData.projects.pagePath}>
+										{routePageData.projects.pageTitle}
+									</Link>
+								</NavbarMenutItem>
+							</NavBar>
+							<OutletContainer />
+						</Layout>
+					}
+				>
+					<Route index element={<About pageTitleProp={routePageData.about.pageTitle} />} />
+					<Route
+						path={routePageData.projects.pagePath}
+						element={<Projects pageTitleProp={routePageData.projects.pageTitle} />}
+					/>
+					<Route
+						path={routePageData.contact.pagePath}
+						element={<Contact pageTitleProp={routePageData.contact.pageTitle} />}
+					/>
+					<Route
+						path={routePageData.noMatch.pagePath}
+						element={<NoMatch pageTitle={routePageData.noMatch.pageTitle} />}
+					/>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
