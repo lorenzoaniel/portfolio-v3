dbef53ea737c563985a536511c82314b6c723b28
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-08 13:05:13 -0600

    Create routesPageData.js
    fake data that simulates API data

---
+++ src/Api/RoutesPageData/routesPageData.js
@@ -0,0 +1,29 @@
+export const routesPageData = {
+	layout: {
+		pagePath: "/",
+		classNameMenuItem: "layout-menuitem",
+	},
+	about: {
+		pageTitle: "About",
+		pagePath: "/",
+		pageGridArea: "about",
+		classNameMenuItem: "about-menuitem",
+	},
+	projects: {
+		pageTitle: "Projects",
+		pagePath: "projects",
+		pageGridArea: "projects",
+		classNameMenuItem: "projects-menuitem",
+	},
+	contact: {
+		pageTitle: "Contact",
+		pagePath: "contact",
+		pageGridArea: "contact",
+		classNameMenuItem: "contact-menuitem",
+	},
+	noMatch: {
+		pageTitle: "404 Not Found",
+		pagePath: "*",
+		classNameMenuItem: "nomatch-menuitem",
+	},
+};
