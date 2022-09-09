deb1235dd8bd17051597d5b14389956be6595361
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-09 16:36:54 -0600

    Update OutletContainer.js
    basic styling will update later

--- src/Components/Outlet/OutletContainer.js
+++ src/Components/Outlet/OutletContainer.js
@@ -5,6 +5,10 @@ import { Outlet } from "react-router";
 const OutletDiv = styled.div`
 	background-color: yellow;
 	grid-area: outlet;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
 `;
 
 export default function OutletContainer() {
