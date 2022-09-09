f0b2cd24ebd5c5fbf454d439e5bba6823952dcf9
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-09 16:36:29 -0600

    Update NavbarMenutItem.js
    styling update

--- src/Components/Navbar/NavbarMenuItem/NavbarMenutItem.js
+++ src/Components/Navbar/NavbarMenuItem/NavbarMenutItem.js
@@ -1,10 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 
-const NavbarMenutItemContainer = styled.div``;
+const NavbarMenutItemContainer = styled.div`
+	font-family: "Bebas Neue", cursive;
+	grid-area: ${(props) => props.gridAreaProp};
+
+	a {
+		text-decoration: none;
+	}
+`;
 
 export default function NavbarMenutItem(props) {
-	const { children, classNameProp } = props;
+	const { children, classNameProp, gridAreaProp } = props;
 
-	return <NavbarMenutItemContainer className={classNameProp}>{children}</NavbarMenutItemContainer>;
+	return (
+		<NavbarMenutItemContainer gridAreaProp={gridAreaProp} className={classNameProp}>
+			{children}
+		</NavbarMenutItemContainer>
+	);
 }
