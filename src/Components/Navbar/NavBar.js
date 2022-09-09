9a0028e01366ebedaf3db3bbc628e99e9b44594f
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-09 16:35:39 -0600

    Update NavBar.js
    added grid templates

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -4,6 +4,8 @@ import styled from "styled-components";
 const NavbarContainer = styled.div`
 	background-color: orange;
 	grid-area: navbar;
+	display: grid;
+	grid-template-areas: "logo about projects contact burgermenu";
 `;
 
 export default function NavBar(props) {
