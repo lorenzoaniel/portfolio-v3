211e295f3f3d23edd1df237c7c246b625f902651
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-09 14:21:27 -0600

    Create navBarData.js
    to separate navBarData from routePageDate

---
+++ src/Api/NavbarData/navBarData.js
@@ -0,0 +1,25 @@
+export const navBarData = {
+	logo: {
+		navbarItemGridArea: "logo",
+		menuItemclassName: "logo-navbaritem",
+	},
+	about: {
+		menuItemTitle: "About",
+		menuItemGridArea: "about",
+		menuItemclassName: "about-menuitem",
+	},
+	projects: {
+		menuItemTitle: "Projects",
+		menuItemGridArea: "projects",
+		menuItemclassName: "projects-menuitem",
+	},
+	contact: {
+		menuItemTitle: "Contact",
+		menuItemGridArea: "contact",
+		menuItemclassName: "contact-menuitem",
+	},
+	burgermenu: {
+		navbarItemGridArea: "burgermenu",
+		menuItemclassName: "burgermenu-navbaritem",
+	},
+};
