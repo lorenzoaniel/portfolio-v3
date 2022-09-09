0c2016411522643f8e3668358ff4f5c44cc63ca0
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-09 16:33:31 -0600

    added grid area and class prop
    refer to title

--- src/Components/Navbar/Logo/Logo.js
+++ src/Components/Navbar/Logo/Logo.js
@@ -2,11 +2,18 @@ import React from "react";
 import styled from "styled-components";
 
 const LogoContainer = styled.div`
+	background-color: blanchedalmond;
 	height: 1vh;
 	width: 1vw;
+
+	grid-area: ${(props) => props.gridAreaProp};
 `;
 
 export default function Logo(props) {
-	const { children } = props;
-	return <LogoContainer>{children}</LogoContainer>;
+	const { children, classNameProp, gridAreaProp } = props;
+	return (
+		<LogoContainer gridAreaProp={gridAreaProp} className={classNameProp}>
+			{children}
+		</LogoContainer>
+	);
 }
