e979006e313a670c20fbb6aace73635e2cf1b617
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-10 19:22:15 -0600

    Update HamburgerMenu.js
    container for hamburger line icons. added basic grid structure and layout will have to work on it to center lines perfectly

--- src/Components/Navbar/HamburgerMenu/HamburgerMenu.js
+++ src/Components/Navbar/HamburgerMenu/HamburgerMenu.js
@@ -3,16 +3,25 @@ import styled from "styled-components";
 
 const HamburgerMenuContainer = styled.div`
 	background-color: blue;
-	height: 3rem;
-	width: 3rem;
+	height: ${(props) => props.dynamicSizeProp};
+	width: ${(props) => props.dynamicSizeProp};
+	margin: 1.5vw 0;
+	margin-right: 1vw;
 
 	grid-area: ${(props) => props.gridAreaProp};
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-rows: repeat(4, 25%);
+	grid-template-areas: "m-1" "m-2" "m-3" "m-4";
+	justify-content: space-around;
+	align-items: center;
 `;
 
 export default function HamburgerMenu(props) {
-	const { children, toggleBurgerMenuProp, classNameProp, gridAreaProp } = props;
+	const { children, toggleBurgerMenuProp, classNameProp, gridAreaProp, dynamicSizeProp } = props;
 	return (
 		<HamburgerMenuContainer
+			dynamicSizeProp={dynamicSizeProp}
 			gridAreaProp={gridAreaProp}
 			className={classNameProp}
 			onClick={() => toggleBurgerMenuProp((curr) => !curr)}
