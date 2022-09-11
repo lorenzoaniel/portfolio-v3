f9f96faf71275ad2f0629f0a410dc381d3271e0b
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-10 19:17:45 -0600

    Update NavBar.js
    added grid structure and layout for children components

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -5,7 +5,12 @@ const NavbarContainer = styled.div`
 	background-color: orange;
 	grid-area: navbar;
 	display: grid;
-	grid-template-areas: "logo about projects contact burgermenu";
+	grid-template-columns: 1fr;
+	grid-template-rows: 1fr;
+	grid-template-areas: "logo . . . . . about projects contact burgermenu";
+	gap: 0 2vw;
+	justify-content: space-around;
+	align-items: center;
 `;
 
 export default function NavBar(props) {
