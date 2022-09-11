249da85f3985e03639ca0dc43f668cff57701bb9
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-10 19:22:45 -0600

    Create hamburgerMenuData.js
    data for the class names and grid names as well as other properties of hamburger menu icons

---
+++ src/Api/NavbarData/HamburgerMenuData/hamburgerMenuData.js
@@ -0,0 +1,18 @@
+export const hamburgerMenuData = {
+	m1: {
+		hamburgerIconGridArea: "m-1",
+		hamburgerIconClassName: "hamburgerIconM1",
+	},
+	m2: {
+		hamburgerIconGridArea: "m-2",
+		hamburgerIconClassName: "hamburgerIconM2",
+	},
+	m3: {
+		hamburgerIconGridArea: "m-3",
+		hamburgerIconClassName: "hamburgerIconM3",
+	},
+	m4: {
+		hamburgerIconGridArea: "m-4",
+		hamburgerIconClassName: "hamburgerIconM4s",
+	},
+};
