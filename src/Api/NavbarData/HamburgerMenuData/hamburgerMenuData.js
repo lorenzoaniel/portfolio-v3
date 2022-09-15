22f1282409323a143be8e36e0803266d51f3c9f3
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-15 14:45:42 -0600

    Added rotate animation values
    used for forming "M" using 4 lines and their proper degrees of rotation

--- src/Api/NavbarData/HamburgerMenuData/hamburgerMenuData.js
+++ src/Api/NavbarData/HamburgerMenuData/hamburgerMenuData.js
@@ -2,17 +2,21 @@ export const hamburgerMenuData = {
 	m1: {
 		hamburgerIconGridArea: "m-1",
 		hamburgerIconClassName: "hamburgerIconM1",
+		hamburgerIconClickAnimationRotateValue: 10,
 	},
 	m2: {
 		hamburgerIconGridArea: "m-2",
 		hamburgerIconClassName: "hamburgerIconM2",
+		hamburgerIconClickAnimationRotateValue: -15,
 	},
 	m3: {
 		hamburgerIconGridArea: "m-3",
 		hamburgerIconClassName: "hamburgerIconM3",
+		hamburgerIconClickAnimationRotateValue: 15,
 	},
 	m4: {
 		hamburgerIconGridArea: "m-4",
 		hamburgerIconClassName: "hamburgerIconM4",
+		hamburgerIconClickAnimationRotateValue: -10,
 	},
 };
