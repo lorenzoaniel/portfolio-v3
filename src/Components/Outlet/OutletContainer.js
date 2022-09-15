a4f75f27c8a4bb05aeb3da14aee791a0efec771a
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-15 17:09:08 -0600

    Update OutletContainer.js
    color and shadows

--- src/Components/Outlet/OutletContainer.js
+++ src/Components/Outlet/OutletContainer.js
@@ -3,12 +3,14 @@ import styled from "styled-components";
 import { Outlet } from "react-router";
 
 const OutletDiv = styled.div`
-	background-color: yellow;
+	background-color: var(--grey-outlet-bg);
 	grid-area: outlet;
 
 	display: flex;
 	justify-content: center;
 	align-items: center;
+
+	box-shadow: var(--black-shadow-inset);
 `;
 
 export default function OutletContainer() {
