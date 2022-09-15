c458aa786503b2e694e07226988d267cef7ed0b9
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-14 19:01:55 -0600

    Update HamburgerMenuItem.js
    added framer motion animation on hover

--- src/Components/Navbar/HamburgerMenu/HamburgerMenuItem/HamburgerMenuItem.js
+++ src/Components/Navbar/HamburgerMenu/HamburgerMenuItem/HamburgerMenuItem.js
@@ -1,13 +1,45 @@
-import React from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
-const HamburgerMenuItemIcon = styled.span`
+const HamburgerMenuItemIcon = styled(motion.span)`
 	background-color: black;
 	display: inline-block;
 	width: 90%;
 	height: 40%;
+	border-radius: 1vw;
+
+	grid-area: ${(props) => props.gridAreaProp};
 `;
 
-export default function HamburgerMenuItem() {
-	return <HamburgerMenuItemIcon />;
+export default function HamburgerMenuItem(props) {
+	const { gridAreaProp, classNameProp } = props;
+
+	const hamburgerMenuItemVariants = {
+		rest: {
+			y: 0,
+			transition: {
+				duration: 0.5,
+				type: "tween",
+				ease: "easeIn",
+			},
+		},
+		hover: {
+			y: "0.5vw",
+			transition: {
+				duration: 0.4,
+				type: "tween",
+				ease: "easeOut",
+				repeat: Infinity,
+				repeatType: "reverse",
+			},
+		},
+	};
+
+	return (
+		<HamburgerMenuItemIcon
+			variants={hamburgerMenuItemVariants}
+			gridAreaProp={gridAreaProp}
+			className={classNameProp}
+		/>
+	);
 }
