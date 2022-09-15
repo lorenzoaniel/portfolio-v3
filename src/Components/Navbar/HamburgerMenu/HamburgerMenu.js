0353830ed3e41a1add5ae9bba2d348d944f652b0
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-14 19:01:39 -0600

    Update HamburgerMenu.js
    added framer motion orchestration via staggerChildren and motion props

--- src/Components/Navbar/HamburgerMenu/HamburgerMenu.js
+++ src/Components/Navbar/HamburgerMenu/HamburgerMenu.js
@@ -1,8 +1,8 @@
-import React from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
-const HamburgerMenuContainer = styled.div`
-	background-color: blue;
+const HamburgerMenuContainer = styled(motion.div)`
+	/* background-color: blue; */
 	height: ${(props) => props.dynamicSizeProp};
 	width: ${(props) => props.dynamicSizeProp};
 	margin: 1.5vw 0;
@@ -13,18 +13,35 @@ const HamburgerMenuContainer = styled.div`
 	grid-template-columns: 1fr;
 	grid-template-rows: repeat(4, 25%);
 	grid-template-areas: "m-1" "m-2" "m-3" "m-4";
-	justify-content: space-around;
 	align-items: center;
 `;
 
 export default function HamburgerMenu(props) {
 	const { children, toggleBurgerMenuProp, classNameProp, gridAreaProp, dynamicSizeProp } = props;
+
+	const HamburgerMenuVariants = {
+		rest: {
+			transition: {
+				staggerChildren: 0.1,
+			},
+		},
+		hover: {
+			transition: {
+				staggerChildren: 0.1,
+			},
+		},
+	};
+
 	return (
 		<HamburgerMenuContainer
 			dynamicSizeProp={dynamicSizeProp}
 			gridAreaProp={gridAreaProp}
 			className={classNameProp}
 			onClick={() => toggleBurgerMenuProp((curr) => !curr)}
+			variants={HamburgerMenuVariants}
+			whileHover={"hover"}
+			initial={"rest"}
+			animate={"rest"}
 		>
 			{children}
 		</HamburgerMenuContainer>
