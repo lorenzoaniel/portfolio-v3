0f2134957db191c930b7bd577317f038d5593706
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-16 18:15:40 -0600

    Added onclick and hover animation FRAMER
    basically scales up on hover and scales down below normal on click, also added smooth transition when unhovering or returning to normal state

--- src/Components/Navbar/NavbarMenuItem/NavbarMenuItem.js
+++ src/Components/Navbar/NavbarMenuItem/NavbarMenuItem.js
@@ -1,26 +1,65 @@
-import React from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
-const NavbarMenuItemContainer = styled.div`
+const NavbarMenuItemContainer = styled(motion.div)`
 	font-family: "Bebas Neue", cursive;
-	grid-area: ${(props) => props.gridAreaProp};
+	grid-area: ${(props) => props.gridareaprop};
 
 	a {
 		text-decoration: none;
-		font-size: ${(props) => props.dynamicSizeProp};
-		color: var(--grey-navbar-items);
+		font-size: ${(props) => props.dynamicsizeprop};
 		text-shadow: var(--darkgrey-shadow-text-menuitems);
+		color: var(--grey-navbar-items);
+	}
+
+	&:hover {
+		a {
+			transition: color 0.5s ease-in-out;
+			color: var(--darkgrey-navbar-hover);
+		}
 	}
 `;
 
 export default function NavbarMenuItem(props) {
-	const { children, classNameProp, gridAreaProp, dynamicSizeProp } = props;
+	const { children, classNameProp, gridareaprop, dynamicsizeprop } = props;
+
+	const NavbarMenuItemVariants = {
+		initial: {
+			scale: 1,
+			transition: {
+				ease: "easeIn",
+				duration: 0.3,
+			},
+		},
+		hover: {
+			scale: 1.1,
+			transition: {
+				ease: "easeInOut",
+				duration: 0.5,
+			},
+		},
+		click: {
+			scale: 0.9,
+			transition: {
+				ease: "easeInOut",
+				duration: 0.1,
+			},
+		},
+	};
+
+	const NavbarMenuItemMotionProps = {
+		variants: NavbarMenuItemVariants,
+		whileHover: NavbarMenuItemVariants.hover,
+		whileTap: NavbarMenuItemVariants.click,
+		animate: NavbarMenuItemVariants.initial,
+	};
 
 	return (
 		<NavbarMenuItemContainer
-			dynamicSizeProp={dynamicSizeProp}
-			gridAreaProp={gridAreaProp}
+			dynamicsizeprop={dynamicsizeprop}
+			gridareaprop={gridareaprop}
 			className={classNameProp}
+			{...NavbarMenuItemMotionProps}
 		>
 			{children}
 		</NavbarMenuItemContainer>
