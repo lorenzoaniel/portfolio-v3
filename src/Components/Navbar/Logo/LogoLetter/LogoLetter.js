d74f38ba741503c4aa7c14b01a0ed3e77337bca8
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-20 19:02:22 -0600

    base look for Logo Letter
    nothing special just added base css for logo letter span

--- src/Components/Navbar/Logo/LogoLetter/LogoLetter.js
+++ src/Components/Navbar/Logo/LogoLetter/LogoLetter.js
@@ -1,11 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
-const LogoLetterSpan = styled.span`
-	font-size: 1vw;
+const LogoLetterSpan = styled(motion.span)`
+	display: inline-block;
+	height: 100%;
+	width: min-content;
+	font-size: ${(props) => props.dynamicsizeprop};
+	font-family: "Bebas Neue", cursive;
+	text-shadow: var(--darkgrey-shadow-text-menuitems);
+	color: var(--grey-navbar-items);
+	display: flex;
+	align-items: center;
 `;
 
 export default function LogoLetter(props) {
-	const { letter } = props;
-	return <LogoLetterSpan>{letter}</LogoLetterSpan>;
+	const { children, dynamicsizeprop } = props;
+
+	return <LogoLetterSpan dynamicsizeprop={dynamicsizeprop}>{children}</LogoLetterSpan>;
 }
