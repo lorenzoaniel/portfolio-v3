f17bf155f61cabc3764d5f1dcfbe98ff68231d11
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-20 19:01:07 -0600

    Update Logo.js
    added unmount animation

--- src/Components/Navbar/Logo/Logo.js
+++ src/Components/Navbar/Logo/Logo.js
@@ -1,24 +1,46 @@
 import React from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
-const LogoContainer = styled.div`
-	background-color: blanchedalmond;
-	height: ${(props) => props.dynamicSizeProp};
-	width: ${(props) => props.dynamicSizeProp};
-	margin: 1.5vw 0;
+const LogoContainer = styled(motion.div)`
+	height: ${(props) => props.dynamicsizeprop};
+	width: 100%;
 	margin-left: 1vw;
 
-	grid-area: ${(props) => props.gridAreaProp};
+	grid-area: ${(props) => props.gridareaprop};
+	display: flex;
+	align-items: center;
 `;
 
 export default function Logo(props) {
-	const { children, classNameProp, gridAreaProp, dynamicSizeProp, toggleLogoProp } = props;
+	const { children, classNameProp, gridareaprop, dynamicsizeprop } = props;
+
+	const LogoVariants = {
+		initial: {
+			opacity: [0, 1],
+			transition: {
+				duration: 0.3,
+			},
+		},
+		exit: {
+			opacity: 0,
+			transition: {
+				duration: 0.3,
+			},
+		},
+	};
+
+	const LogoMotionProps = {
+		animate: LogoVariants.initial,
+		exit: LogoVariants.exit,
+	};
+
 	return (
 		<LogoContainer
-			dynamicSizeProp={dynamicSizeProp}
-			gridAreaProp={gridAreaProp}
+			dynamicsizeprop={dynamicsizeprop}
+			gridareaprop={gridareaprop}
 			className={classNameProp}
-			onClick={toggleLogoProp}
+			{...LogoMotionProps}
 		>
 			{children}
 		</LogoContainer>
