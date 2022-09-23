8ae6623f8cc382dee97a639b51e3d2579f1320eb
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-22 20:09:47 -0600

    Create AboutSubMenuItemData.js
    data for about submenu

---
+++ src/Api/RoutesPageData/About/AboutSubMenuItemData/AboutSubMenuItemData.js
@@ -0,0 +1,16 @@
+const AboutSubMenuItemData = {
+	summary: {
+		title: "Summary",
+		submenuitemclass: "submenu-summary",
+	},
+	skills: {
+		title: "Skills",
+		submenuitemclass: "submenu-skills",
+	},
+	certification: {
+		title: "Certification",
+		submenuitemclass: "submenu-certification",
+	},
+};
+
+export default AboutSubMenuItemData;
