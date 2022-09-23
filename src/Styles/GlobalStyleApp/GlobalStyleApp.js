eac19e0bc2522b5986c403398ef6455a15387b65
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-22 20:27:13 -0600

    added more color and shadow variables
    added more color and shadow variables

--- src/Styles/GlobalStyleApp/GlobalStyleApp.js
+++ src/Styles/GlobalStyleApp/GlobalStyleApp.js
@@ -8,10 +8,33 @@ export const GlobalStyleApp = createGlobalStyle`
     
     --grey-navbar-items: rgba(222, 226, 230, 1);
     --darkgrey-navbar-hover: rgba(108, 117, 125, 1);
-    
+
+    --lightgrey-outlet-font: rgba(248,249,250,1);
+    --darkgrey-outlet-bg: rgba(33,37,41,1);
+
+    --grey-outlet-maintitle: rgba(222, 226, 230, 1);
+    --grey-outlet-sectitle: rgba(233, 236, 239, 1);
+    --darkgrey-outlet-maintitle-container: rgba(52, 58, 64, 1);
+    --darkgrey-outlet-sectitle-container: rgba(73, 80, 87, 1);
+
+    --grey-outlet-submenuitem-title: rgba(233, 236, 239, 1);
+    --grey-outlet-submenuitem-bg: rgba(73,80,87,1);
+    --grey-outlet-submenu-bg: rgba(52, 58, 64, 1);
+
     /*SHADOWS*/
     --black-shadow-inset: 0 0 1rem 0.1rem rgba(0, 0, 0, 0.8) inset;
-    --darkgrey-shadow-text-menuitems: 0 0.5rem 0.2rem rgba(33, 37, 41, 1);  
+    --black-shadow-outset: 0 0 0.3rem 0.1rem rgba(0, 0, 0, 0.5);
+    --darkgrey-shadow-text-menuitems: 0 0.5rem 0.2rem rgba(33, 37, 41, 1);
+    
+    --black-shadow-outletmain-outset: 0 0 0.3rem 0.1rem rgba(0, 0, 0, 0.8);
+    --black-shadow-outletmain-inset: 0 0 0.3rem 0.1rem rgba(0, 0, 0, 0.8) inset;
+
+    --black-shadow-outletmaintitle-outset: 0 0 0.3rem 0.1rem rgba(0, 0, 0, 0.5);
+
+    --darkgrey-shadow-submenu-menuitems: 0 0.3rem 0.2rem rgba(0, 0, 0, 1); 
+
+    --darkgrey-shadow-submenu-menu-outset: 0 0 0.7rem 0.3rem rgba(0, 0, 0, 0.5);
+    --darkgrey-shadow-submenu-menu-inset: 0 0 0.1rem 0.1rem rgba(33, 37, 41, 1) inset;
 
 
     padding: 0;
