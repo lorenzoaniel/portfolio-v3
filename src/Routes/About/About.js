640517d02918dc357f295e3121f36fcfba1bcb45
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-22 20:26:34 -0600

    Added titles, submenus, main board
    3 major components added which added the main area for info as well as the submenus to switch what info is displayed on the main area, also added the main title and secondary label underneath( currently has my name as well as front end developer title)

--- src/Routes/About/About.js
+++ src/Routes/About/About.js
@@ -1,13 +1,43 @@
 import React from "react";
 import styled from "styled-components";
+
+import AboutMain from "../../Components/Outlet/About/AboutMain/AboutMain";
+import AboutMainSubMenu from "../../Components/Outlet/About/AboutMainSubMenu/AboutMainSubMenu";
+import AboutMainTitle from "../../Components/Outlet/About/AboutMainTitle/AboutMainTitle";
+import AboutMainSubMenuItem from "../../Components/Outlet/About/AboutMainSubMenu/AboutMainSubMenuItem/AboutMainSubMenuItem";
+
+import { nanoid } from "nanoid";
+
 import { routePageBaseStyle } from "../../Styles/RoutePageBaseStyle/routePageBaseStyle";
 
 const AboutContainer = styled.div`
 	${routePageBaseStyle}
+
+	display: grid;
+	grid-template-columns: repeat(5, 20%);
+	grid-template-rows: 10% 45% 45%;
+	grid-template-areas: "maintitle maintitle maintitle . ." "submenu main main main main" ". main main main main"; // add the other row
 `;
 
-export default function About(props) {
-	const { pageTitleProp, classNameProp } = props;
+const createMainSubMenuItems = (subMenuData) => {
+	return Object.keys(subMenuData).map((curr) => {
+		return (
+			<AboutMainSubMenuItem
+				titleprops={subMenuData[curr].title}
+				key={nanoid()}
+			></AboutMainSubMenuItem>
+		);
+	});
+};
 
-	return <AboutContainer className={classNameProp}>{pageTitleProp}</AboutContainer>;
+export default function About(props) {
+	const { classNameProp, subMenuDatasProp } = props;
+
+	return (
+		<AboutContainer className={classNameProp}>
+			<AboutMainTitle></AboutMainTitle>
+			<AboutMainSubMenu>{createMainSubMenuItems(subMenuDatasProp)}</AboutMainSubMenu>
+			<AboutMain></AboutMain>
+		</AboutContainer>
+	);
 }
