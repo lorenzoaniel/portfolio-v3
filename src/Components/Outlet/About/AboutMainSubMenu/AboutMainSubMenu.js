f812f393b090c8f29dea348549dce8eafc476551
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-22 20:22:20 -0600

    Create AboutMainSubMenu.js
    contains submenu items, made sure this only takes half of the grid rows to create a folder tab effect

---
+++ src/Components/Outlet/About/AboutMainSubMenu/AboutMainSubMenu.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+const AboutMainSubMenuContainer = styled.div`
+	background-color: var(--grey-outlet-submenu-bg);
+	grid-area: submenu;
+
+	height: 75%;
+	width: 120%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-items: flex-end;
+
+	border-bottom-left-radius: 1rem;
+	border-top-left-radius: 1rem;
+
+	box-shadow: var(--black-shadow-outletmain-outset), var(--black-shadow-outletmain-inset);
+`;
+
+function AboutMainSubMenu(props) {
+	const { children } = props;
+
+	return <AboutMainSubMenuContainer>{children}</AboutMainSubMenuContainer>;
+}
+
+export default AboutMainSubMenu;
