31748365377462b0c2c436fc8dac5f92d27af9c3
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-27 13:01:18 -0600

    Update Layout.js
    replaced with injected style

--- src/Routes/Layout/Layout.js
+++ src/Routes/Layout/Layout.js
@@ -3,13 +3,6 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 const LayoutContainer = styled(motion.div)`
-	/* height: inherit;
-	width: inherit;
-	display: grid;
-	grid-template-columns: 100%;
-	grid-template-rows: min-content minmax(min-content, 1fr);
-	grid-template-areas: "navbar" "outlet"; */
-
 	${(props) => props.layoutstyleprop}
 `;
 
