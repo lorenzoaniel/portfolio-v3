6814255a2fac53f5dfd3e263cc2b7ee9c30b4800
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-28 13:41:55 -0600

    Update Summary.js
    added css to main container, decided to to treat summary container as the parent for other children components since there is a lot of information that will be displayed here.

--- src/Routes/About/AboutMain/Summary.js
+++ src/Routes/About/AboutMain/Summary.js
@@ -1,12 +1,47 @@
 import React from "react";
 import styled from "styled-components";
+import AboutMainAvatar from "../../../Components/Outlet/About/AboutMainAvatar/AboutMainAvatar";
 import { AboutOutletBaseStyle } from "../../../Styles/About/AboutOutletBaseStyle/AboutOutletBaseStyle";
 
 const SummaryContainer = styled.div`
 	${AboutOutletBaseStyle};
+	display: grid;
+	grid-template-columns: 100%;
+	grid-template-rows: 1fr 1fr;
+	grid-template-areas: "avatarcontainer" "summaryintroduction";
+	align-items: center;
 `;
 
-export default function Summary() {
-	// const { children } = props;
-	return <SummaryContainer>Summary</SummaryContainer>;
+const SummaryIntroductionContainer = styled.div`
+	width: 95%;
+	height: 100%;
+	justify-self: center;
+	grid-area: summaryintroduction;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	overflow-wrap: break-word;
+	padding: 0.5vw;
+`;
+
+const SummaryIntroductionSpan = styled.span`
+	font-size: clamp(1rem, 0.7319rem + 1.1594vw, 2.5rem);
+`;
+
+export default function Summary(props) {
+	const { summaryprop, avatarsrcprop, avataraltprop, dynamicsizeprop } = props;
+	return (
+		<SummaryContainer>
+			<AboutMainAvatar
+				imgsrcprop={avatarsrcprop}
+				imgaltprop={avataraltprop}
+				dynamicsizeprop={dynamicsizeprop}
+			/>
+			<SummaryIntroductionContainer>
+				<SummaryIntroductionSpan dynamicsizeprop={dynamicsizeprop}>
+					{summaryprop}
+				</SummaryIntroductionSpan>
+			</SummaryIntroductionContainer>
+		</SummaryContainer>
+	);
 }
