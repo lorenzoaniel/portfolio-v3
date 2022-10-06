ef38f55d8d4148b16cc73ea57e4d9c3db3124bea
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-05 18:01:10 -0600

    Update Socials.js
    added a create function for making social containers for each link and icon

--- src/Routes/About/AboutMain/Socials.js
+++ src/Routes/About/AboutMain/Socials.js
@@ -1,12 +1,31 @@
 import React from "react";
 import styled from "styled-components";
+import AboutMainSocial from "../../../Components/Outlet/About/AboutMainSocial/AboutMainSocial";
 import { AboutOutletBaseStyle } from "../../../Styles/About/AboutOutletBaseStyle/AboutOutletBaseStyle";
+import { nanoid } from "nanoid";
 
 const SocialsContainer = styled.div`
 	${AboutOutletBaseStyle};
+	display: flex;
+	justify-content: space-around;
+	align-content: flex-start;
+	flex-wrap: wrap;
 `;
 
-export default function Socials() {
-	// const { children } = props;
-	return <SocialsContainer>Socials</SocialsContainer>;
+const createAboutMainSocial = (socialdata) => {
+	return Object.keys(socialdata).map((currSocial) => {
+		return (
+			<AboutMainSocial
+				descriptionprops={socialdata[currSocial].description}
+				socialiconprops={socialdata[currSocial].icon}
+				linkprops={socialdata[currSocial].link}
+				key={nanoid()}
+			/>
+		);
+	});
+};
+
+export default function Socials(props) {
+	const { socialdataprops } = props;
+	return <SocialsContainer>{createAboutMainSocial(socialdataprops)}</SocialsContainer>;
 }
