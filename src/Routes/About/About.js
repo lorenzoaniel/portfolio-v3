ff6aeb738b1f87ddc1cbae52b5d9b98f7f0c9c53
lorenzoaniel <lorenzoaniel@gmail.com>
2022-10-10 16:13:00 -0600

    Update About.js
    transtition between pages animation, simple fade effect

--- src/Routes/About/About.js
+++ src/Routes/About/About.js
@@ -1,6 +1,7 @@
 import { React } from "react";
 import { Outlet } from "react-router";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 import AboutMain from "../../Components/Outlet/About/AboutMain/AboutMain";
 import AboutMainSubMenu from "../../Components/Outlet/About/AboutMainSubMenu/AboutMainSubMenu";
@@ -10,15 +11,6 @@ import AboutMainSubMenuItem from "../../Components/Outlet/About/AboutMainSubMenu
 import { nanoid } from "nanoid";
 
 import { routePageBaseStyle } from "../../Styles/RoutePageBaseStyle/routePageBaseStyle";
-
-const AboutContainer = styled.div`
-	${routePageBaseStyle}
-
-	display: grid;
-	grid-template-columns: repeat(5, 20%);
-	grid-template-rows: 10% 45% 45%;
-	grid-template-areas: "maintitle maintitle maintitle . ." "submenu main main main main" ". main main main main"; // add the other row
-`;
 
 const createMainSubMenuItems = (subMenuData) => {
 	return Object.keys(subMenuData).map((curr) => {
@@ -33,11 +25,35 @@ const createMainSubMenuItems = (subMenuData) => {
 	});
 };
 
+const AboutContainerMotionVariants = {
+	initial: {
+		opacity: 0,
+		transition: {
+			duration: 0.3,
+		},
+	},
+	onload: {
+		opacity: 1,
+	},
+	exit: {
+		opacity: 0,
+		transition: {
+			duration: 0.3,
+		},
+	},
+};
+
+const AboutContainerMotionProps = {
+	initial: AboutContainerMotionVariants.initial,
+	animate: AboutContainerMotionVariants.onload,
+	exit: AboutContainerMotionVariants.exit,
+};
+
 export default function About(props) {
 	const { classNameProp, subMenuDatasProp } = props;
 
 	return (
-		<AboutContainer className={classNameProp}>
+		<AboutContainer className={classNameProp} {...AboutContainerMotionProps}>
 			<AboutMainTitle></AboutMainTitle>
 			<AboutMainSubMenu>{createMainSubMenuItems(subMenuDatasProp)}</AboutMainSubMenu>
 			<AboutMain>
@@ -46,3 +62,12 @@ export default function About(props) {
 		</AboutContainer>
 	);
 }
+
+const AboutContainer = styled(motion.div)`
+	${routePageBaseStyle}
+
+	display: grid;
+	grid-template-columns: repeat(5, 20%);
+	grid-template-rows: 10% 45% 45%;
+	grid-template-areas: "maintitle maintitle maintitle . ." "submenu main main main main" ". main main main main";
+`;
